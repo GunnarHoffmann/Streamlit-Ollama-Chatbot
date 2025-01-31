f909bce6ea79f489bacd2c615aa009a7d33cb23b
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2025-01-31 10:16:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ https://www.youtube.com/@DevTechBytes
 class Config:
     PAGE_TITLE = "Streamlit Ollama Chatbot"
 
-    OLLAMA_MODELS = ('codellama:7b')
+    OLLAMA_MODELS = ('codellama:7b','deepseek-r1:14b')
 
     SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following 
                     open-source models {OLLAMA_MODELS}.
