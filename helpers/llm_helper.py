7892d075abe23964ecd44b4dd3f23a4df1d33121
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2025-01-31 10:11:34 +0100

    Update llm_helper.py

--- helpers/llm_helper.py
+++ helpers/llm_helper.py
@@ -12,8 +12,15 @@ system_prompt = Config.SYSTEM_PROMPT
 # Specify the API endpoint
 os.environ['OLLAMA_HOST'] = 'http://172.205.182.197:11434'
 
+
+from ollama import Client
+client = Client(
+  host='http://172.205.182.197:11434',
+  headers={'x-some-header': 'some-value'}
+)
+
 def chat(user_prompt, model):
-    stream = ollama.chat(
+    stream = client.chat(
         model=model,
         messages=[{'role': 'assistant', 'content': system_prompt},
                   {'role': 'user', 'content': f"Model being used is {model}.{user_prompt}"}],
